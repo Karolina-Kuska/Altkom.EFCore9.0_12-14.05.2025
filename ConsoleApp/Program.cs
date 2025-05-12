d04e5639007a97eb20edcb6a1fd12a5fb4adc4dd
Paweł Łęczycki <pawel@msd.info.pl>
2025-05-12 11:35:17 +0200

    DbContext

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using DAL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+configurationBuilder.AddJsonFile("appsettings.json");
+IConfiguration configuration = configurationBuilder.Build();
+
+//var connectionString = "Server=(local);Database=EF;Integrated security=true;TrustServerCertificate=true";
+
+
+
+using (var context = ContextWithDependencyInjection(configuration))
+{
+    context.Database.EnsureDeleted();
+}
+
+using (var context = ContextWithDbContextOptions(configuration))
+{
+    context.Database.EnsureCreated();
+}
+
+
+
+
+static MyContext ContextWithDependencyInjection(IConfiguration configuration)
+{
+    var serviceCollection = new ServiceCollection();
+    serviceCollection.AddDbContext<MyContext>(options =>
+        options.UseSqlServer(configuration.GetConnectionString(nameof(MyContext))));
+
+    var serviceProvider = serviceCollection.BuildServiceProvider();
+
+    return serviceProvider.GetRequiredService<MyContext>();
+}
+
+static MyContext ContextWithConnectionString(IConfiguration configuration)
+{
+    var connectionString = configuration.GetConnectionString("MyContext")!;
+    return new MyContext(connectionString);
+}
+
+static MyContext ContextWithDbContextOptions(IConfiguration configuration)
+{
+    var connectionString = configuration.GetConnectionString("MyContext")!;
+
+    var builder = new DbContextOptionsBuilder<MyContext>();
+    var options = builder.UseSqlServer(connectionString)
+        .Options;
+
+    return new MyContext(options);
+}
---
+++ DAL/MyContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    public class MyContext : DbContext
+    {
+        private readonly string? _connectionString;
+
+        //konstruktor z parametrem connectionString
+        public MyContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        //konstruktor z parametrem DbContextOptions - do użycia z DI
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            //jeśli nie skonfigurowano opcji, to dodajemy domyślną konfigurację na podstawie connectionString
+            if (!optionsBuilder.IsConfigured && _connectionString is not null)
+            {
+                optionsBuilder.UseSqlServer(_connectionString);
+            }
+        }
+
+    }
+}
